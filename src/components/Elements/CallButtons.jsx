52712197aae8f14fe8e7fdc542c1d4243066918b
AlicanKurus <alicankurus.dev@gmail.com>
2025-06-10 15:25:30 +0200

    nav ok

---
+++ src/components/Elements/CallButtons.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CallButtons = () => {
+   
+  return (
+    <div style={{display:'flex'}}>
+      <div className="left"  ></div>
+      <div className="right" ></div>
+
+
+    </div>
+  )
+}
+
+export default CallButtons
--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -4,22 +4,26 @@ import TopNavbar from "../components/Nav/TopNavbar";
 import Header from "../components/Sections/Header";
 import Services from "../components/Sections/Services";
 import Projects from "../components/Sections/Projects";
-import Blog from "../components/Sections/Blog";
+// import Blog from "../components/Sections/Blog";
 import Pricing from "../components/Sections/Pricing";
 import Contact from "../components/Sections/Contact";
 import Footer from "../components/Sections/Footer"
+import CallButtons from "../components/Elements/CallButtons";
 
 export default function Landing() {
   return (
     <>
+
       <TopNavbar />
       <Header />
       <Services />
       <Projects />
-      <Blog />
+      {/* <Blog /> */}
       <Pricing />
       <Contact />
       <Footer />
+      <CallButtons />
+
     </>
   );
 }
